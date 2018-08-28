25e4e869ae2c2135da7f8f5d74ea9b39c23e1657
ganjahnavarro <ganjahnavarro@gmail.com>
2018-08-29 05:27:17 +0800

    added program repository class. temporary product availment change

--- src/main/java/com/gnjb/fitly/model/product/ProductAvailment.java
+++ src/main/java/com/gnjb/fitly/model/product/ProductAvailment.java
@@ -1,23 +1,5 @@
 package com.gnjb.fitly.model.product;
 
-import javax.persistence.Entity;
-import javax.persistence.Transient;
-
-import core.model.Record;
-
-@Entity(name = ProductAvailment.ENTITY_NAME)
-public class ProductAvailment extends Record {
-
-	private static final long serialVersionUID = 6093613995199631190L;
-	public static final String ENTITY_NAME = "productAvailment";
-	
-	// TODO
-
-	@Override
-	@Transient
-	public String getDisplayString() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public class ProductAvailment {
 
 }
---
+++ src/main/java/com/gnjb/fitly/model/product/ProgramRepository.java
@@ -0,0 +1,32 @@
+package com.gnjb.fitly.model.product;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import core.repository.AbstractRepository;
+
+@Repository
+@Transactional
+public class ProgramRepository extends AbstractRepository<Program> {
+
+	@SuppressWarnings("unchecked")
+	public List<Program> findFilteredItems(String filter, Integer pageSize, Integer pageOffset, String orderBy) {
+		Criteria criteria = getPagedItemsCriteria(pageSize, pageOffset, orderBy);
+
+		if (filter != null && !filter.isEmpty()) {
+			criteria.add(Restrictions.ilike("name", filter, MatchMode.START));
+		}
+		return criteria.list();
+	}
+
+	@Override
+	protected String getEntityName() {
+		return Program.ENTITY_NAME;
+	}
+
+}
