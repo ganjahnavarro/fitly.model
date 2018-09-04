bb92a13cc128446cc8af6a2fa7a6dee7bf1622d6
ganjahnavarro <ganjahnavarro@gmail.com>
2018-09-05 05:48:52 +0800

    renamed constants class

--- src/main/java/core/FitlyConstants.java
+++ src/main/java/core/FitlyConstants.java
@@ -1,6 +1,6 @@
 package core;
 
-public final class Constants {
+public final class FitlyConstants {
 
 	public static final String CONFIG_NAME_MEMBERSHIP_AMOUNT = "Membership Amount";
 	public static final String CONFIG_VALUE_MEMBERSHIP_AMOUNT = "1000";
