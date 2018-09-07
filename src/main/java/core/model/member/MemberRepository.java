6f5691652db1ce955f4b85f5023e4bd968ffcfcb
ganjahnavarro <ganjahnavarro@gmail.com>
2018-09-07 11:52:30 +0800

    added find members count. added find all memberships.

--- src/main/java/core/model/member/MemberRepository.java
+++ src/main/java/core/model/member/MemberRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.hibernate.Criteria;
 import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +15,17 @@ import core.repository.AbstractRepository;
 @Repository
 @Transactional
 public class MemberRepository extends AbstractRepository<Member> {
+	
+	@SuppressWarnings("unchecked")
+	public Long findCount(MemberType type) {
+		Criteria criteria = getDefaultCriteria();
+
+		type = type == null ? MemberType.REGULAR : type;
+		criteria.add(Restrictions.eq("type", type));
+
+		List<Member> list = criteria.list();
+		return list != null ? Long.valueOf(list.size()) : 0;
+	}
 
 	@SuppressWarnings("unchecked")
 	public List<Member> findFilteredItems(MemberType type, String filter, Integer pageSize, Integer pageOffset, String orderBy) {
@@ -42,6 +54,14 @@ public class MemberRepository extends AbstractRepository<Member> {
 		Criteria criteria = getSession().createCriteria(Membership.class);
 		criteria.add(Restrictions.eq("member.id", memberId));
 		return (Membership) criteria.uniqueResult();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Membership> findAllMemberships() {
+		Criteria criteria = getSession().createCriteria(Membership.class);
+		criteria.addOrder(Order.desc("startDate"));
+		criteria.addOrder(Order.desc("modifiedDate"));
+		return criteria.list();
 	}
 
 	@Override
